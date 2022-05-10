406fc72a8959c984aff9b2a6f2476886404ab780
ec <ec@dlmail.co.uk>
2022-05-10 16:56:59 +0100

    feat: improved convertImages

--- src/utilities/convertImage.ts
+++ src/utilities/convertImage.ts
@@ -4,15 +4,12 @@ import path from 'path';
 const convertImageFolder: string = '../converted_images';
 const convertImagePath = path.resolve(__dirname, convertImageFolder);
 
-const convertImages = async (images: string[]): Promise<void> => {
+const convertImages = async (images: string[], width: number, height: number): Promise<void> => {
   images.forEach(async (image) => {
-    console.log(`image: ${image} being converted`);
     await sharp(image)
-      .resize(200, 200)
-      .toFile(`${convertImagePath}/200_200-${image.split('/').at(-1)}`);
-    //   .toFile(`${path.dirname(image)}/200_200-${image.split('/').at(-1)}`);
+      .resize(width, height)
+      .toFile(`${convertImagePath}/${width}-${height}-${image.split('/').at(-1)}`);
   });
-  console.log(process.cwd());
 };
 
 export { convertImages };
