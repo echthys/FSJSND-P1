e5fd5db3546ab775893c91ac539c9b9fac7cb820
ec <ec@dlmail.co.uk>
2022-05-10 16:57:47 +0100

    feat: can now take query params

--- src/routes/api/index.ts
+++ src/routes/api/index.ts
@@ -6,8 +6,11 @@ const routes = express.Router();
 const imageDir: string = '../../images';
 
 routes.get('/api', async (req, res) => {
-  const images = await findImages(imageDir);
-  convertImages(images);
+  const params = req.query;
+  const width: number = parseInt(params.width as string) ? parseInt(params.width as string) as number : 200;
+  const height: number = parseInt(params.height as string) ? parseInt(params.height as string) as number : 200;
+  const images: string[] = await findImages(imageDir);
+  convertImages(images, width, height);
   res.sendStatus(200);
 });
 
